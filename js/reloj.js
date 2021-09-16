ab1fcfcb4852d3b57e4be7430ed6566f51523901
Andy <andysatie@gmail.com>
2021-09-16 11:33:45 -0300

    se maquetó el reloj

---
+++ js/reloj.js
@@ -0,0 +1,12 @@
+let date = new Date();
+console.log(date);
+console.log(date.getMonth()); //Comienza desde cero porque devuelve un Array
+console.log(date.getDay()); //numero del día de la semana (arranca Domingo=0, Lunes=1)
+console.log(date.getDate()); //numero del dia (16)
+console.log(date.getFullYear()); //devuelve el año
+//hay metodos para obtener horas, minutos y segundos.
+
+let fecha = document.querySelector('#fecha')
+let hora = document.querySelector('#hora')
+console.log(fecha);
+console.log(hora);
